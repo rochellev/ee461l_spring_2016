8163583e4f4391d07790027ee4a979c92af4dbcb
Nico Cortes <nico.crts@gmail.com>
2016-03-23 21:32:03 -0500

    - Successfully added facebook api to the project - TODO login - TODO chat UI - TODO Sending/Receiving Tests - TODO Store messages if facebook api doesn't already do this

--- DungeonManager/app/src/main/java/com/dunman/dungeonmanager/PlayerActivity.java
+++ DungeonManager/app/src/main/java/com/dunman/dungeonmanager/PlayerActivity.java
@@ -5,30 +5,23 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 
+import com.facebook.FacebookCallback;
 import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
+import com.facebook.appevents.AppEventsLogger;  
 
-public class MainActivity extends AppCompatActivity {
-
+public class PlayerActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
